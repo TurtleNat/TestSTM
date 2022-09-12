04427eebf41a73b65194f45828f17a12d538814c
Полуян Наталья Вечеславовна <poluyannv@ad.atlant.by>
2022-09-12 17:38:50 +0300

    добавлен проект.

---
+++ Core/Inc/led_control.h
@@ -0,0 +1,20 @@
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __LED_CONTROL_H
+#define __LED_CONTROL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void led_on(void);
+
+void led_off(void);
+
+void led_toggle(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LED_CONTROL_H */
